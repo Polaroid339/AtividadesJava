64f0b97316d39d7284120323a4faf1796fa71225
Polaroid339 <victor339335@gmail.com>
2025-03-15 10:42:50 -0300

    feat: Atividades Java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 7;
+        int num3 = 6;
+
+        double media = (num1 + num2 +num3)/3.0;
+        int media2 = (num1 + num2 +num3)/3;
+        long media3 = (num1 + num2 +num3)/3L;
+
+        System.out.println("A média do double é: " + media);
+        System.out.println("A média do inteiro é: " + media2);
+        System.out.println("A média do long é: " + media3);
+        System.out.println();
+    }
+}
